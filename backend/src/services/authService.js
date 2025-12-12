bd2d56daa48d2d373ddd85c7b97906d695a4dc33
Katrina de Guzman <katrinavdeguzman@gmail.com>
2025-12-11 23:48:07 -0800

    Login user

--- backend/src/services/authService.js
+++ backend/src/services/authService.js
@@ -34,24 +34,28 @@ export default {
   },
 
   async login({ username, password }) {
-    // TODO: get ahold of the db using readDb();
-    // TODO: check the database for a user with a matching username and password
-    // TODO: if there is no user:
-    //       - construct a new Error("Invalid username or password");
-    //       - set the statusCode of that error object to 401
-    //       - throw the err
-    // TODO: otherwise, create a login token. I'll help you out with this one:
-    // const token = jwt.sign({ userId: user.id, username: user.username }, JWT_SECRET, { expiresIn: "1h" })
-    // TODO:  return an object that contains 2 things:
-    //  - token
-    //  - user : { id: user.id, username: user.username, profilePicture: user.profilePicture }
-
+    
+    const db = await readDb();
+    
+    const user = db.users.find((u) => u.username === username && u.password === password);
+    if (!user) {
+      const err = new Error("Invalid username or password");
+      err.statusCode = 401;
+      throw err;
+    }
+    
+    const token = jwt.sign(
+        {userId: user.id, username: user.username},
+        JWT_SECRET,
+        {expiresIn: "1h"}
+    )
+    
     return {
       token,
       user: {
-        id: "dummy-id",
-        username: "dummy-username",
-        profilePicture: "dummy-profilePicture",
+        id: user.id,
+        username: user.username,
+        profilePicture: user.profilePicture,
       },
     };
   },
